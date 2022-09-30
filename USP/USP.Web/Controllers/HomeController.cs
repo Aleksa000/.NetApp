9ecb751ee40f38155a6d8063290cae31d864056c
AleksaGrbic4 <aleksa.grbic.19@singimail.rs>
2022-09-30 21:10:23 +0200

    v1

--- USP/USP.Web/Controllers/HomeController.cs
+++ USP/USP.Web/Controllers/HomeController.cs
@@ -15,9 +15,58 @@ public class HomeController : Controller//:nasledjivanje metode
 
     public IActionResult Index()
     {
+        
         return View();//vraca neki html
     }
+    /// <summary>
+    /// get all users
+    /// </summary>
+    /// <returns></returns>
+    [HttpGet]
+    public IActionResult Users()
+    {
+        var userModel = new UserModel//prepoznaje sam tip podataka o kome se radi
+        {
+            Id = 1,
+            UserName = "Aleksa1",
+            FirstName = "Aleksa",
+            LastName = "Grbic",
+            Email = "aleksa.grbic00@gmail.com",
+        };
+        var userModel2 = new UserModel//prepoznaje sam tip podataka o kome se radi
+        {
+            Id = 2,
+            UserName = "Aleksa2",
+            FirstName = "Aleksa",
+            LastName = "Savic",
+            Email = "aleksa.savic00@gmail.com",
+        };
 
+        var listOfUserModel = new List<UserModel>();
+        
+        listOfUserModel.Add(userModel);
+        listOfUserModel.Add(userModel2);
+        
+        return View(listOfUserModel);//vraca neki html
+    }
+    /// <summary>
+    /// Create user - post metod
+    /// </summary>
+    /// <returns></returns>
+    [HttpPost]
+    public IActionResult CreateUser(UserModel model)
+    {
+        return View();
+    }
+    /// <summary>
+    /// Create user get metodom
+    /// </summary>
+    /// <returns></returns>
+    [HttpGet]
+    public IActionResult CreateUser()
+    {
+        return View();
+    }
     public IActionResult Privacy()
     {
         return View();
